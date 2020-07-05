d7db796233e3667bb2b7e15b167d9c98cbdeb22e
Ashutosh Bhatta <ashutoshb469@gmail.com>
2020-07-06 00:06:53 +0545

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+document.querySelector('.btn-primary').addEventListener('click',()=>{
+    document.querySelector('.btn-primary').textContent = 'Beginner';
+});
+
+document.querySelector('.btn-py').addEventListener('click',()=>{
+    document.querySelector('.btn-py').textContent = 'Intermediate';
+});
+
+document.querySelector('.btn-css').addEventListener('click',()=>{
+    document.querySelector('.btn-css').textContent = 'Intermediate';
+});
+
+document.querySelector('.btn-html').addEventListener('click',()=>{
+    document.querySelector('.btn-html').textContent = 'Intermediate';
+});
