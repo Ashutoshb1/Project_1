ec2399b556f9cb389fdf9d3af84ef4ba0be138ca
Ashutosh Bhatta <ashutoshb469@gmail.com>
2020-07-08 11:03:09 +0545

    Second commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,20 @@
-document.querySelector('.btn-primary').addEventListener('click',()=>{
+document.querySelector('.btn-primary').addEventListener('mouseover',()=>{
     document.querySelector('.btn-primary').textContent = 'Beginner';
+    document.querySelector('.skills h3').classList.add('animate__pulse');
+    
 });
 
-document.querySelector('.btn-py').addEventListener('click',()=>{
+document.querySelector('.btn-py').addEventListener('mouseover',()=>{
     document.querySelector('.btn-py').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__headShake');
 });
 
-document.querySelector('.btn-css').addEventListener('click',()=>{
+document.querySelector('.btn-css').addEventListener('mouseover',()=>{
     document.querySelector('.btn-css').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__pulse');
 });
 
-document.querySelector('.btn-html').addEventListener('click',()=>{
+document.querySelector('.btn-html').addEventListener('mouseover',()=>{
     document.querySelector('.btn-html').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__pulse');
 });
