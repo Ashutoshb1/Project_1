eefb8407157877006fc04d09cdaf3abd2fdb3627
Ashutosh Bhatta <ashutoshb469@gmail.com>
2020-07-09 12:06:32 +0545

    Improved the layout

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,85 @@
 document.querySelector('.btn-primary').addEventListener('mouseover',()=>{
-    document.querySelector('.btn-primary').textContent = 'Beginner';
-    document.querySelector('.skills h3').classList.add('animate__pulse');
-    
+    document.querySelector('.btn-primary').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__pulse'); 
 });
 
 document.querySelector('.btn-py').addEventListener('mouseover',()=>{
     document.querySelector('.btn-py').textContent = 'Intermediate';
     document.querySelector('.skills h3').classList.add('animate__headShake');
 });
+document.querySelector('.btn-py').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-py').textContent = 'Python';
+});
+
 
 document.querySelector('.btn-css').addEventListener('mouseover',()=>{
     document.querySelector('.btn-css').textContent = 'Intermediate';
     document.querySelector('.skills h3').classList.add('animate__pulse');
 });
+document.querySelector('.btn-css').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-css').textContent = 'CSS';
+});
+
 
 document.querySelector('.btn-html').addEventListener('mouseover',()=>{
     document.querySelector('.btn-html').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__bounceIn');
+});
+document.querySelector('.btn-html').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-html').textContent = 'HTML';
+});
+
+
+document.querySelector('.btn-tf').addEventListener('mouseover',()=>{
+    document.querySelector('.btn-tf').textContent = 'Beginner';
     document.querySelector('.skills h3').classList.add('animate__pulse');
+});
+document.querySelector('.btn-tf').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-tf').textContent = 'Tensorflow';
+});
+
+document.querySelector('.btn-vivado').addEventListener('mouseover',()=>{
+    document.querySelector('.btn-vivado').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__jello');
+});
+document.querySelector('.btn-vivado').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-vivado').textContent = 'Vivado';
+});
+
+
+
+
+document.querySelector('.btn-sql').addEventListener('mouseover',()=>{
+    document.querySelector('.btn-sql').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__pulse');
+});
+document.querySelector('.btn-sql').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-sql').textContent = 'SQL';
+});
+
+
+document.querySelector('.btn-verilog').addEventListener('mouseover',()=>{
+    document.querySelector('.btn-verilog').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__pulse');
+});
+document.querySelector('.btn-verilog').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-verilog').textContent = 'verilog';
+});
+
+
+document.querySelector('.btn-matlab').addEventListener('mouseover',()=>{
+    document.querySelector('.btn-matlab').textContent = 'Intermediate';
+    document.querySelector('.skills h3').classList.add('animate__pulse');
+});
+document.querySelector('.btn-matlab').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-matlab').textContent = 'MATLAB';
+});
+
+
+document.querySelector('.btn-hive').addEventListener('mouseover',()=>{
+    document.querySelector('.btn-hive').textContent = 'Beginner';
+    document.querySelector('.skills h3').classList.add('animate__pulse');
+});
+document.querySelector('.btn-hive').addEventListener('mouseleave',()=>{
+    document.querySelector('.btn-hive').textContent = 'HIVE';
 });
